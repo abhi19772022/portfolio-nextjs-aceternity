16dffe92d7e7a90218b8ef4bdb3f1879c48a68a1
Lalan Chaudhary <chaudharylalan28@gmail.com>
2024-08-29 16:35:35 +0530

    Link Preview

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,8 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "assets.aceternity.com" },
-      { protocol: "https", hostname: "aceternity.com" },
-      { protocol: "https", hostname: "pbs.twimg.com" },
+      { protocol: "https", hostname: "**" },
+      
     ],
   },
 };
